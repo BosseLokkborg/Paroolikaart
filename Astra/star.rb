2b54cd178cee512d8c8342c22387aca6907f1403
BosseLokkborg <79745734+BosseLokkborg@users.noreply.github.com>
2021-03-01 10:56:08 +0200

    Update star.rb

--- Astra/star.rb
+++ Astra/star.rb
@@ -1,8 +1,9 @@
-module Idc
-  class Wallet
-    attr_accessor :owner_id, :balance
-  end
-  #suunan syntaksi umber "key" on siin hashi nimi
-  end
+#from here I want to require some code
+#all this code down here is for an example, I just want to understand what am I missing, why isn't this working
+#all variable, module, class names are named using Name or var and a number for example sake
 
-#rahakott => wallet 
+module Name 
+  var1 = "Some text I wish to print out"
+  var2 = 10
+ var3 = "John Doe"
+end 
