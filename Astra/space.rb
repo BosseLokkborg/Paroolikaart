f35cd3020dd901ee910dcbebbb7a806da7a8d0b3
BosseLokkborg <79745734+BosseLokkborg@users.noreply.github.com>
2021-03-01 11:02:32 +0200

    Update space.rb

--- Astra/space.rb
+++ Astra/space.rb
@@ -1,4 +1,5 @@
 require_relative 'star.rb'
+include "Name"
 #also I have tried using $LOAD_PATH.unshift File.dirname(__FILE__), but with no luck either
 require 'ruby2d'
 
@@ -9,11 +10,13 @@ Image.new('idClogo.png',
   width: 75, height: 75,
 x: 175, y:0)
 
-Text.new("Your name is" + ,
+
+Text.new("Your name is" + var3,
   x: 0, y: 75,
 size: 15,
 color: 'black')
-
+#this is where it gives me an error, that var3 is uninitialised, btw I have also tried it without modules, only variables.
+#I have tried using @ infront of variables(I was trying many different options), but again nothing
 
 
 show
