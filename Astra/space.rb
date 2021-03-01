b52ddb70813bba2d2a8e53668c8d478f6497e5b6
BosseLokkborg <79745734+BosseLokkborg@users.noreply.github.com>
2021-03-01 10:59:33 +0200

    Update space.rb

--- Astra/space.rb
+++ Astra/space.rb
@@ -1,23 +1,19 @@
-require 'ruby2d'
 require_relative 'star.rb'
+#also I have tried using $LOAD_PATH.unshift File.dirname(__FILE__), but with no luck either
+require 'ruby2d'
 
-
-
-set title: 'idCoin - test wallet'
+set title: "Test1"
 set background: 'white', width: 250, height: 400
-
 
 Image.new('idClogo.png',
   width: 75, height: 75,
 x: 175, y:0)
 
-Text.new("Sinu vaba saldo on: 0",
+Text.new("Your name is" + ,
   x: 0, y: 75,
 size: 15,
 color: 'black')
 
-Image.new('idClogo.png',
-width: 35, height: 35,
-x: 150, y: 75)
+
 
 show
