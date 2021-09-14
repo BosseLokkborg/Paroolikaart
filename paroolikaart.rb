db41ee09d722e984c03da8f2cd0c9c86f315c60a
BosseLokkborg <79745734+BosseLokkborg@users.noreply.github.com>
2021-09-14 17:16:24 +0300

    Add files via upload

---
+++ paroolikaart.rb
@@ -0,0 +1,39 @@
+b = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+"A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f",
+"G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l",
+"M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r",
+"S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x",
+"Y", "y", "Z", "z"] #71
+n = b 
+# lihtsam
+m = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+"A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f",
+"G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l",
+"M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r",
+"S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x",
+"Y", "y", "Z", "z"] #61
+
+
+#raskem
+l = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", ".", "?", "_", "/",
+"A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f",
+"G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l",
+"M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r",
+"S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x",
+"Y", "y", "Z", "z"] #68
+
+v = 71
+puts ("A:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " B:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" C:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " D:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" E:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " F:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" G:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + "\nH:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" I:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " J:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" K:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" L:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " M:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+"\nN:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " O:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" P:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " Q:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" R:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " S:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+"\nT:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " U:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" V:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " W:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" X:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s + " Y:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s +
+" Z:" + n[rand(0...v)].to_s + n[rand(0...v)].to_s + n[rand(0...v)].to_s)
