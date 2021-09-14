9ab6c50df3c9c0268bde7113ffc5ab8f593ed52f
BosseLokkborg <79745734+BosseLokkborg@users.noreply.github.com>
2021-09-14 17:17:17 +0300

    Delete Astra directory

--- Astra/star.rb
+++
@@ -1,9 +0,0 @@
-#from here I want to require some code
-#all this code down here is for an example, I just want to understand what am I missing, why isn't this working
-#all variable, module, class names are named using Name or var and a number for example sake
-
-module Name 
-  var1 = "Some text I wish to print out"
-  var2 = 10
- var3 = "John Doe"
-end 
